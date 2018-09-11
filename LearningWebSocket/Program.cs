ad35a805f3ed710c98ff4c99e138f592f9f8e230
MonBjo LW <m.bjork95@gmail.com>
2018-09-11 09:50:14 +0200

    Trying to get a hang of this.

---
+++ LearningWebSocket/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+
+namespace LearningWebSocket {
+    public class Program {
+
+        public static void Main() {
+            // Defining data
+            string ipAdress = "127.0.0.1";
+            int portNumber = 80;
+
+            TcpListener server = new TcpListener(IPAddress.Parse(ipAdress), portNumber);
+            server.Start();
+            Console.WriteLine($"Server started on {ipAdress}{Environment.NewLine}Waiting for connection...");
+
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("A client connected.");
+
+            //Socket serverSocket = new Socket(AddressFamily.Unspecified, SocketType.Stream, ProtocolType.IP); // hmm, what.
+        }
+
+        public void Write(byte[] buffer, int offset, int size) {
+
+        }
+
+        public void Read(byte[] buffer, int offset, int size) {
+
+        }
+    }
+}
